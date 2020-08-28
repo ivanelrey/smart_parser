6763fd63953635207db6a3510c61b9731e76bb61
Ivane Gkomarteli <ivanejg@yahoo.com>
2020-08-28 16:42:09 +0100

    Raise ArgumentError if file with file name doesn't exist

--- application.rb
+++ application.rb
@@ -1,5 +1,6 @@
 class Application
   def run(file_name)
     raise ArgumentError, 'Please provide a file name.' if file_name.nil?
+    raise ArgumentError, 'Please provide an existing file.' unless File.exist?(file_name)
   end
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -2,9 +2,17 @@ RSpec.describe Application do
   let(:app) { described_class.new }
 
   describe '#run' do
-    context 'without file name' do
+    context 'without file_name' do
       it 'raises an argument error with correct message' do
         expect { app.run(nil) }.to raise_error(ArgumentError, 'Please provide a file name.')
+      end
+    end
+
+    context 'when file with file_name doesn\'t exist' do
+      let(:ivalid_file_name) { 'not_existing_file.log' }
+
+      it 'raises an argument error with correct message' do
+        expect { app.run(ivalid_file_name) }.to raise_error(ArgumentError, 'Please provide an existing file.')
       end
     end
   end
